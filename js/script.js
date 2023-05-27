7b115481c23f3b51f722f99a8a65fd9e8ba2a880
DarrenDonovan <donovandarren12@gmail.com>
2023-05-27 15:08:34 +0700

    Blog edit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("showCommentsButton").addEventListener("click", function() {
+    var commentsdiv = document.getElementById("commentsdiv");
+    if (commentsdiv.style.display === "none") {
+      commentsdiv.style.display = "block";
+    } else {
+      commentsdiv.style.display = "none";
+    }
+  });
+  
